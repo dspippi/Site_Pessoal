eeb995ddc1f7844a61c0764758b533683355b05b
dspippi <103321598+dspippi@users.noreply.github.com>
2022-04-16 19:47:13 -0300

    Som adicionado.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 
+embassarATela();
+playMP3();
 
+function embassarATela(){
 document.addEventListener('scroll',function(){
     let s = window.scrollY;
     let opacityVal = (s/550.0);
     console.log(opacityVal)
     this.querySelector(".blurred").style.opacity=1.0-opacityVal;
 })
+}
+
+function playMP3(){
+var vid = document.getElementById("aud");
+vid.volume = 0.05;
+vid.play();}
